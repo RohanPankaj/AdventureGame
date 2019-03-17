cd7f545d08f92301870d16b612ce4a9bde075825
Rohan Pankaj <rohansp01@gmail.com>
2019-03-16 22:53:32 -0700

    Added background images and started making code more functional

--- main.py
+++ main.py
@@ -1,38 +1,72 @@
 import pygame
 
 pygame.init()
-
-window = pygame.display.set_mode((500, 500))
+screenWidth = 1000
+screenLength = 1000
+window = pygame.display.set_mode((screenLength, screenWidth))
 pygame.display.set_caption("Adventure Game")
 
+walk = [pygame.image.load("standing.PNG"),pygame.image.load("Walking1.PNG")]
+background = pygame.image.load("background.PNG")
+clock = pygame.time.Clock()
 x = 50
 y = 50
 width = 40
 height = 60
 vel = 5
 
+left = False
+right = False
+walk = False
+walkCount = 0
+
+
+
 run = True
 
+def redrawGameWindow():
+    global walkCount
+
+    window.blit(background, (0,0))
+    if walkCount + 1 > 27:
+        walkCount = 0
+    
+    if walk:
+        window.blit(walk[walkCount//2], (x, y))
+        walkCount += 1
+    '''else:
+        window.blit(walk[walkCount], (x, y))
+        walkCount += 1'''
+    pygame.display.update()
 while run:
-    pygame.time.delay(100)
+    clock.tick(6)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
     controls = pygame.key.get_pressed()
-    screenWidth = 500
-    screenLength = 500
+    
     if controls[pygame.K_RIGHT] and x < screenWidth - width - vel:
         x += vel
-    if controls[pygame.K_LEFT] and x > vel:
+        right = True
+        left = False
+    elif controls[pygame.K_LEFT] and x > vel:
         x -= vel
+        left = True
+        right = False
+    else:
+        right = False
+        left = False
+        walkCount = 0
     if controls[pygame.K_UP] and y > vel:
         y -= vel
+        right = False
+        left = False
     if controls[pygame.K_DOWN] and y < screenLength - height - vel:
         y += vel
-
-    window.fill((0, 0, 0))
-    pygame.draw.rect(window, (255, 255, 255), (x, y, width, height))
-    pygame.display.update()
+        right = False
+        LEft = False
+    redrawGameWindow()
+    
 pygame.quit()

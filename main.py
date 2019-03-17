5b9a41e5fbecdb4f9e6a427dc7d5f20f8605ab14
Rohan Pankaj <rohansp01@gmail.com>
2019-03-16 20:10:26 -0700

    added limit so sprite does not go off the screen

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ pygame.init()
 window = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("Adventure Game")
 
-#Sprtie Height and velocity
 x = 50
 y = 50
 width = 40
@@ -14,29 +13,26 @@ vel = 5
 
 run = True
 
-#Getting what keyboard keys are being pressed
-controls = pygame.key.get_pressed()
-
-#Main Loop
 while run:
     pygame.time.delay(100)
-    #Close program when x clicked
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False\
-    #Movement Controls            
-    if controls[pygame.K_RIGHT]:
+            run = False
+
+    controls = pygame.key.get_pressed()
+    screenWidth = 500
+    screenLength = 500
+    if controls[pygame.K_RIGHT] and x < screenWidth - width - vel:
         x += vel
-    if controls[pygame.K_LEFT]:
+    if controls[pygame.K_LEFT] and x > vel:
         x -= vel
-    if controls[pygame.K_UP]:
+    if controls[pygame.K_UP] and y > vel:
         y -= vel
-    if controls[pygame.K_DOWN]:
+    if controls[pygame.K_DOWN] and y < screenLength - height - vel:
         y += vel
-    #reset window so sprtie looks like it is moving
+
     window.fill((0, 0, 0))
-    #Set siprite color and size
     pygame.draw.rect(window, (255, 255, 255), (x, y, width, height))
-    #Update screen
     pygame.display.update()
 pygame.quit()
